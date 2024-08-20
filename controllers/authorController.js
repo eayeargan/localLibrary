00252f7dc2998826e72daa5c91128a9303450653
Alexandre Yeargar <eayeargan@gmail.com>
2024-08-20 08:20:10 -0400

    adding controllers and views for updates

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -121,9 +121,24 @@ exports.author_delete_post = asyncHandler(async(req, res, next)=>{
         res.redirect("/catalog/authors");
     }
 });
-
+// first name, family name, date of birth, date of death
 exports.author_update_get = asyncHandler(async(req, res, next)=>{
-    res.send('NOT IMPLEMENTED: Author update GET');
+    const [author, allBooksByAuthor] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({
+            author: req.params.id
+        }, "title summary").exec()
+    ]);
+    if (author === null){
+        const err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+    }
+    res.render("author_form", {
+        title: "Update Author",
+        all_books: allBooksByAuthor,
+        author: author
+    });
 });
 
 exports.author_update_post = asyncHandler(async(req,res, next)=>{
