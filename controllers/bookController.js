7dfe547b0a90aa9452b469ed3d045ee497ab85c3
Alexandre Yeargar <eayeargan@gmail.com>
2024-09-12 18:51:34 -0400

    committing controllers folder

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,7 +8,7 @@ const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async(req, res, next)=>{
     const [numBooks, numBookInstances, numAvailableBookInstances, numAuthors, numGenres] = await Promise.all([Book.countDocuments({}).exec(), BookInstance.countDocuments({}).exec(), BookInstance.countDocuments({status: "Available"}).exec(), Author.countDocuments({}).exec(), Genre.countDocuments({}).exec()]);
-    res.render("index", {title: "Local Library Home", book_count: numBooks, book_instance_count: numBookInstances, book_instance_available_count: numAvailableBookInstances, author_count: numAuthors, genre_count: numGenres});
+    res.render("index", {title: "Harvey Hurricane's Local Library Home", book_count: numBooks, book_instance_count: numBookInstances, book_instance_available_count: numAvailableBookInstances, author_count: numAuthors, genre_count: numGenres});
 });
 
 // Display list of all books.
